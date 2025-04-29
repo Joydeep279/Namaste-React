1a42aff3b5bb68f0b1ebc254a45a87e244250b3d
Joydeep Nath <joydeepnath279@gmail.com>
2025-04-29 08:57:14 +0530

    day 1 DONE

--- Inception/script.js
+++ Inception/script.js
@@ -1,16 +1,12 @@
-const element = document.querySelector(".root");
-const btn = document.createElement("button");
-const countElement = document.createElement("span");
-countElement.style.display = "block";
-countElement.style.maxWidth = "5rem";
-countElement.style.textAlign = "center";
-
-btn.textContent = "CLICKED 0 Times";
-element.appendChild(countElement);
-element.appendChild(btn);
-let count = 0;
-countElement.textContent = count;
-btn.addEventListener("click", () => {
-  btn.textContent = `CLICK ME ${++count} Times`;
-  countElement.textContent = count;
-});
+const root = ReactDOM.createRoot(document.getElementsByClassName("root")[0]);
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "Welcome To React"),
+    React.createElement("h2", { id: "heading" }, "Welcome To Namaste React"),
+  ])
+);
+console.log(parent.type);
+console.log(root);
+root.render(parent);
