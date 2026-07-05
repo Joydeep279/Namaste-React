6c6bd7da2b4cd866ac2a378992412f02c4f29fce
Joydeep Nath <joydeepnath279@gmail.com>
2026-07-05 14:54:09 +0530

    feat: search funtionality

--- Laying the Foundation/src/components/Body.jsx
+++ Laying the Foundation/src/components/Body.jsx
@@ -4,8 +4,12 @@ import Card from "./CardLayout";
 const Body = () => {
   console.log("ReRenderd!!");
 
+  const [searchText, setSearchText] = useState("");
   const [resList, setResList] = useState([]);
+  const [mainList, setMainList] = useState([]);
+
   const [isFiltered, setIsFiltered] = useState(false);
+  const [isShowingSearch, setIsShowingSearch] = useState(true);
 
   useEffect(() => {
     getApiData();
@@ -16,29 +20,47 @@ const Body = () => {
       "https://corsproxy.io/?url=https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5454&lng=88.3600&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
     );
     const json = await data.json();
-
-    setResList(
+    const resData =
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
-        ?.restaurants,
-    );
+        ?.restaurants;
+    setMainList(resData);
+    setResList(resData);
   }
 
   function filterRes() {
     if (isFiltered) {
-      setResList(data);
+      setResList(mainList);
       setIsFiltered(false);
-      return;
+    } else {
+      const filteredList = mainList.filter((item) => item.info.avgRating > 4.5);
+      setResList(filteredList);
+      setIsFiltered(true);
     }
-    const filteredList = resList.filter((item) => item.info.avgRating > 4.1);
-    setResList(filteredList);
-    setIsFiltered(true);
+  }
+
+  function searchRest() {
+    if (isShowingSearch) {
+      setResList(
+        mainList.filter((items) =>
+          items.info.name.toLowerCase().includes(searchText),
+        ),
+      );
+    }
   }
 
   return (
     <div className="Main-Container">
       <div className="Search-Container">
-        <input type="text" placeholder="Search..." id="searchInput" />
-        <button id="searchBtn">Search</button>
+        <input
+          type="text"
+          placeholder="Search..."
+          id="searchInput"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button id="searchBtn" onClick={searchRest}>
+          Search
+        </button>
         <button id="topRated" onClick={filterRes}>
           {isFiltered ? "Back" : "Top Rated Restaurants"}
         </button>
